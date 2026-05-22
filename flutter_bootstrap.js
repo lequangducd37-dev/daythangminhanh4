11c4368953239af9b5d45fac7ebc26710549e247
Le Quang Duc <lequangducd37@gmail.com>
2026-05-22 12:13:20 +0700

    Optimize link preview

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"425cfb54d01a9472b3e81d9e76fd63a4a44cfb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2982546256" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "242302536" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
